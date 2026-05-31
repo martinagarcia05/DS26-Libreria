1277e76e4040f89c4532458855e577a9de9d4523
martinagarcia05 <marti.garcia.amendola@gmail.com>
2026-05-31 00:27:42 -0300

    agrego componentes bootstrap: container y nav en header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,22 +1,26 @@
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../../assets/styles/Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <div className="container header-container">
-        <div className="logo">
-          <img src="/src/assets/libroIcono.png" alt="Logo" className="logo-icon" />
-          <h1>Librería UTN</h1>
-        </div>
-        <nav>
-          <NavLink to="/" className="nav-link">Inicio</NavLink>
-          <NavLink to="/catalogo" className="nav-link">Catálogo</NavLink>
-          <NavLink to="/contacto" className="nav-link">Nosotros</NavLink>
-          <button className="btn-login">Ingresar</button>
-        </nav>
-      </div>
-    </header>
+    <Navbar expand="lg" className="custom-header">
+      <Container>
+        <Navbar.Brand href="/" className="d-flex align-items-center gap-2">
+          <img src="/src/assets/libroIcono.png" alt="Logo" width="32" height="32" />
+          <span className="fw-bold brand-text">Librería UTN</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto align-items-lg-center gap-2">
+            <Nav.Link as={NavLink} to="/" end>Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to="/catalogo">Catálogo</Nav.Link>
+            <Nav.Link as={NavLink} to="/contacto">Nosotros</Nav.Link>
+            <button className="btn-login ms-lg-3 mt-2 mt-lg-0">Ingresar</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
