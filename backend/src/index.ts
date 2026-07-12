eb55fc7a3b01069f433e74add95ab0c5fd57fd79
martinagarcia05 <marti.garcia.amendola@gmail.com>
2026-07-12 13:28:31 -0300

    be c14

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,57 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ mensaje: "API de la Librería — ¡hola desde un contenedor! 🐳" });
+  res.json({ mensaje: "API de la Librería — ¡hola desde un container! 🐳" });
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
+interface Libro {
+  id: number;
+  titulo: string;
+  autor: string;
+  precio: number;
+  imagen: string;
+  disponible: boolean;
+}
+
+interface Autor {
+  id: number;
+  nombre: string;
+  nacionalidad: string;
+}
+
+const libros: Libro[] = [
+  { id: 1, titulo: "El principito", autor: "Antoine de Saint-Exupéry",
+    precio: 4500, imagen: "https://...", disponible: false },
+  { id: 2, titulo: "100 años de soledad", autor: "Gabriel García Márquez",
+    precio: 6000, imagen: "https://...", disponible: true },
+  { id: 3, titulo: "Cien años de soledad", autor: "Gabriel García Márquez",
+    precio: 6000, imagen: "https://...", disponible: false },
+  { id: 4, titulo: "El túnel", autor: "Ernesto Sabato",
+    precio: 5000, imagen: "https://...", disponible: true },
+];
+
+const autores: Autor[] = [
+  { id: 1, nombre: "Antoine de Saint-Exupéry", nacionalidad: "Francia" },
+  { id: 2, nombre: "Gabriel García Márquez", nacionalidad: "Colombia" },
+  { id: 3, nombre: "Ernesto Sabato", nacionalidad: "Argentina" },
+];
+
+app.get("/libros", (req, res) => {
+  const { disponible } = req.query;
+  if (disponible == undefined) {
+    res.json(libros);
+    return;
+  } else if (disponible || disponible === "false") {
+    res.json(libros.filter(libro => libro.disponible === (disponible === "true")));
+  } else {
+    res.json(libros);
+  }
+});
+
+app.get("/autores", (_req, res) => {
+  res.json(autores);
 });
