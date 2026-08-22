c11c067ae1f234600bfbcc84d31ad8b1868665c7
martinagarcia05 <marti.garcia.amendola@gmail.com>
2026-08-22 20:59:36 -0300

    Paso 7:errorHandler y borrado de los try/catch

--- backend/src/controllers/autor.controller.ts
+++ backend/src/controllers/autor.controller.ts
@@ -6,40 +6,24 @@ export async function getAll(req: Request, res: Response) {
 }
 
 export async function getById(req: Request, res: Response) {
-    try {
     const autor = await autoresService.findById(Number(req.params.id));
     if (!autor) return res.status(404).json({ error: "Autor no encontrado" });
     return res.json(autor);
-    } catch (error) {
-        return res.status(500).json({ error: "Error interno del servidor" });
-    }
 }
 
 export async function create(req: Request, res: Response) {
-    try {
         const nuevo = await autoresService.create(req.body);
         return res.status(200).json(nuevo);
-    } catch (error) {
-        return res.status(500).json({ error: "Error interno del servidor" });
-    }
 }
 
 export async function update(req: Request, res: Response) {
-    try {
         const actualizado = await autoresService.update(Number(req.params.id), req.body);
         if (!actualizado) return res.status(404).json({ error: "Autor no encontrado" });
         return res.json(actualizado);
-    } catch (error) {
-        return res.status(500).json({ error: "Error interno del servidor" });
-    }
 }
 
 export async function remove(req: Request, res: Response) {
-    try {
         const borrado = await autoresService.remove(Number(req.params.id));
         if (!borrado) return res.status(404).json({ error: "Autor no encontrado" });
         return res.status(204).send();
-    } catch (error) {
-        return res.status(500).json({ error: "Error interno del servidor" });
-    }
 }
